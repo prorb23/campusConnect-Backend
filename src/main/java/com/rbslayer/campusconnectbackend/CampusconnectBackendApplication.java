5cf0c3679c0f3f7ab8bd2b3cc727d4fe286aa796
Ronit Bhattacharjee <bronit381@gmail.com>
2026-02-04 13:08:27 +0530

    first commit

---
+++ src/main/java/com/rbslayer/campusconnectbackend/CampusconnectBackendApplication.java
@@ -0,0 +1,13 @@
+package com.rbslayer.campusconnectbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusconnectBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampusconnectBackendApplication.class, args);
+	}
+
+}
